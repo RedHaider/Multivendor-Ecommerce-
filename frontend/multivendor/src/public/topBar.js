d4691bade3e7c81b238c07894f05e59493b431fa
RedHaider <syedaliredwanulhaider13@gmail.com>
2024-10-27 09:27:48 +0600

    made changes to frontend

--- frontend/multivendor/src/public/topBar.js
+++ frontend/multivendor/src/public/topBar.js
@@ -3,7 +3,7 @@ import Logout from '../utils/signout';
 
 const TopBar = () => {
     return ( 
-        <div className="topdiv" style={{ backgroundColor: '#eb6626' }}>
+        <div className="topdiv" >
             <div className=" mr-5 ml-5 p-0 d-flex justify-content-between">
                 <img src="picture/logo.svg" alt="Logo" className="logo"/>
                 <Logout />
